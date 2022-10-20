7363a6bda4d8f328c539563e5c32e529125ec095
jeffbone95 <jeffbone95@gmail.com>
2022-10-19 18:40:16 -0600

    progress2

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const cityNameEl=document.getElementById("cityName")
 
 const currentTemperature=document.getElementById("currentTemperature") 
 const citySearchName=document.getElementById("citySearchName") 
-const searchButton=document.getElementById("searchButton")
+const searchButton=document.getElementById("searchButton") 
+const windSpeed=document.getElementById("windSpeed") 
+const relativeHumidity=document.getElementById("relativeHumidity")
 
 function getWeather(){ 
     const city=citySearchName.value  
@@ -20,8 +22,10 @@ function getWeather(){
         console.log(data) 
         cityNameEl.textContent=data.data[0].city_name 
         currentTemperature.textContent=data.data[0].temp 
+        windSpeed.textContent=data.data[0].wind_spd 
+        relativeHumidity.textContent=data.data[0].rh
         
     }) 
     
 }
-searchButton.addEventListener("click", getWeather)
+searchButton.addEventListener("click", getWeather) 
